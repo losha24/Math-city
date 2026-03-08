43a595feb4c6de113ea74cb09cb1b297ed5f155b
losha24 <losha24@gmail.com>
2026-03-08 19:43:15 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,34 +1,21 @@
-const CACHE_NAME="math-city-v1.1"
+const CACHE_NAME = "math-city-v1.3";
+const urls = [
+  "./",
+  "./index.html",
+  "./icon.png",
+  "./logo.png"
+];
 
-const urls=[
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urls))
+  );
+});
 
-"./",
-"./index.html",
-"./icon.png",
-"./logo.png"
-
-]
-
-self.addEventListener("install",e=>{
-
-e.waitUntil(
-
-caches.open(CACHE_NAME)
-
-.then(cache=>cache.addAll(urls))
-
-)
-
-})
-
-self.addEventListener("fetch",e=>{
-
-e.respondWith(
-
-caches.match(e.request)
-
-.then(res=>res||fetch(e.request))
-
-)
-
-})
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(res => res || fetch(e.request))
+  );
+});
