97f7b34284f7fbc00b5ce93800b5de05904a2f05
losha24 <losha24@gmail.com>
2026-03-09 00:57:44 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,67 +1,17 @@
-const CACHE="math-city-v3"
-
-const urls=[
-
-"/",
-"/index.html",
-"/style.css",
-"/icon.png",
-"/logo.png"
-
-]
+const CACHE="math-city-v1";
+const urls=["/","/index.html","/style.css","/icon.png","/logo.png"];
 
 self.addEventListener("install",event=>{
-
-self.skipWaiting()
-
-event.waitUntil(
-
-caches.open(CACHE).then(cache=>{
-
-return cache.addAll(urls)
-
-})
-
-)
-
-})
+self.skipWaiting();
+event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(urls)));
+});
 
 self.addEventListener("activate",event=>{
-
-event.waitUntil(
-
-caches.keys().then(keys=>{
-
-return Promise.all(
-
-keys.map(key=>{
-
-if(key!==CACHE){
-
-return caches.delete(key)
-
-}
-
-})
-
-)
-
-})
-
-)
-
-})
+event.waitUntil(caches.keys().then(keys=>{
+return Promise.all(keys.map(key=>{if(key!==CACHE){return caches.delete(key);}}));
+}));
+});
 
 self.addEventListener("fetch",event=>{
-
-event.respondWith(
-
-caches.match(event.request).then(res=>{
-
-return res || fetch(event.request)
-
-})
-
-)
-
-})
+event.respondWith(caches.match(event.request).then(res=>res||fetch(event.request)));
+});
