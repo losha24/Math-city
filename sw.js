5bd7c49d7b888e4dcd64a7bb7003fcfa429512db
losha24 <losha24@gmail.com>
2026-03-09 01:44:31 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "math-city-v1";
+const urls = [
+  "./",
+  "./index.html",
+  "./icon.png",
+  "./logo.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urls))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(res => res || fetch(e.request))
+  );
+});
