833ca463b6cea6000687ed7bd50650e6e25e6c9a
losha24 <losha24@gmail.com>
2026-03-09 19:53:07 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,55 @@
-const CACHE="math-city-v1";
+const CACHE_NAME = "mathcity-v1.0.2";
 
-self.addEventListener("install",e=>{
+self.addEventListener("install", e => {
+
+self.skipWaiting();
+
 e.waitUntil(
-caches.open(CACHE).then(cache=>{
+
+caches.open(CACHE_NAME).then(cache => {
+
 return cache.addAll([
 "/",
-"/index.html"
-])
-})
-)
+"/index.html",
+"/manifest.json",
+"/logo.png",
+"/icon.png"
+]);
+
 })
 
-self.addEventListener("fetch",e=>{
-e.respondWith(
-caches.match(e.request).then(r=>{
-return r || fetch(e.request)
+);
+
+});
+
+self.addEventListener("activate", event => {
+
+event.waitUntil(
+
+caches.keys().then(keys => {
+
+return Promise.all(
+
+keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+
+);
+
 })
-)
+
+);
+
+});
+
+self.addEventListener("fetch", event => {
+
+event.respondWith(
+
+caches.match(event.request).then(res => {
+
+return res || fetch(event.request);
+
 })
+
+);
+
+});
