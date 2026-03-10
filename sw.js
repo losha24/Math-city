6f14d1e204a01ae5c738cd85f8e471d73bdd2c8c
losha24 <losha24@gmail.com>
2026-03-10 19:57:04 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,37 @@
+const CACHE_NAME="mathcity-v4"
+
+const urls=[
+"./",
+"./index.html",
+"./style.css",
+"./game.js",
+"./logo.png"
+]
+
 self.addEventListener("install",e=>{
 
-self.skipWaiting()
+e.waitUntil(
+
+caches.open(CACHE_NAME).then(cache=>{
+
+return cache.addAll(urls)
+
+})
+
+)
 
 })
 
 self.addEventListener("fetch",e=>{
 
-e.respondWith(fetch(e.request))
+e.respondWith(
+
+caches.match(e.request).then(res=>{
+
+return res||fetch(e.request)
+
+})
+
+)
 
 })
