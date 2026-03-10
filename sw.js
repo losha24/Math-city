2fd03fd29f607da537e282d98af7aa2fde1be361
losha24 <losha24@gmail.com>
2026-03-10 19:32:29 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,11 @@
-const cacheName="mathcity-cache-v4";
-const filesToCache=["/","/index.html","/style.css","/game.js","/sw.js","/version.json","/logo.png"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(cacheName).then(c=>c.addAll(filesToCache)));});
-self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(resp=>resp||fetch(e.request)));});
+self.addEventListener("install",e=>{
+
+self.skipWaiting()
+
+})
+
+self.addEventListener("fetch",e=>{
+
+e.respondWith(fetch(e.request))
+
+})
