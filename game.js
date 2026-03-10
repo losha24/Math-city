d7cf19465a6663678aa8ae31ab85854bb4f04880
losha24 <losha24@gmail.com>
2026-03-10 19:50:02 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,10 +1,202 @@
-fetch("version.json")
-.then(r=>r.json())
-.then(v=>{
+let level=1
+let score=0
+let coins=0
 
-document.getElementById("appVersion").innerText=v.version
+let correctAnswer=0
 
-})
+let rewards=[]
+
+function showScreen(id){
+
+document.querySelectorAll(".screen").forEach(s=>s.classList.remove("active"))
+
+document.getElementById(id).classList.add("active")
+
+}
+
+function startGame(){
+
+showScreen("gameScreen")
+
+newQuestion()
+
+}
+
+function backGame(){
+
+showScreen("gameScreen")
+
+}
+
+function openShop(){
+
+renderShop()
+
+showScreen("shopScreen")
+
+}
+
+function openRewards(){
+
+document.getElementById("rewardList").innerText=rewards.join(" ")
+
+showScreen("rewardsScreen")
+
+}
+
+function openStats(){
+
+document.getElementById("statsBox").innerHTML=
+
+"נקודות: "+score+"<br>מטבעות: "+coins
+
+showScreen("statsScreen")
+
+}
+
+function newQuestion(){
+
+let a=Math.floor(Math.random()*10)
+
+let b=Math.floor(Math.random()*10)
+
+correctAnswer=a+b
+
+document.getElementById("question").innerText=a+" + "+b
+
+}
+
+function check(){
+
+let input=document.getElementById("answer")
+
+let val=parseInt(input.value)
+
+if(val==correctAnswer){
+
+score++
+
+coins+=5
+
+input.className="correct"
+
+build()
+
+}else{
+
+input.className="wrong"
+
+}
+
+updateUI()
+
+setTimeout(()=>{
+
+input.value=""
+
+input.className=""
+
+newQuestion()
+
+},500)
+
+}
+
+function build(){
+
+let el=document.createElement("div")
+
+el.innerText="🏠"
+
+document.getElementById("city").appendChild(el)
+
+}
+
+function renderShop(){
+
+let shop=document.getElementById("shopList")
+
+shop.innerHTML=""
+
+for(let i=0;i<6;i++){
+
+shop.innerHTML+=`
+
+<div class="shopItem">
+
+🏠 בית
+
+<br>
+
+מחיר: 50
+
+<br>
+
+<button onclick="buy()">קנה</button>
+
+</div>
+
+`
+
+}
+
+}
+
+function buy(){
+
+if(coins<50){
+
+alert("אין מספיק מטבעות")
+
+return
+
+}
+
+coins-=50
+
+build()
+
+updateUI()
+
+}
+
+function spinWheel(){
+
+let prizes=[10,20,50,"פרס"]
+
+let p=prizes[Math.floor(Math.random()*prizes.length)]
+
+alert("זכית: "+p)
+
+if(typeof p=="number") coins+=p
+
+else rewards.push("🎁")
+
+updateUI()
+
+}
+
+function parentMode(){
+
+alert("מצב הורים בפיתוח")
+
+}
+
+function updateUI(){
+
+document.getElementById("score").innerText=score
+document.getElementById("coins").innerText=coins
+document.getElementById("level").innerText=level
+
+}
+
+function resetGame(){
+
+localStorage.clear()
+
+location.reload()
+
+}
 
 function updateVersion(){
 
@@ -12,19 +204,13 @@ fetch("version.json?"+Date.now())
 
 .then(r=>r.json())
 
-.then(server=>{
+.then(v=>{
 
 let current=document.getElementById("appVersion").innerText
 
-if(server.version!=current){
+if(v.version!=current){
 
 alert("נמצאה גרסה חדשה")
-
-caches.keys().then(names=>{
-
-names.forEach(n=>caches.delete(n))
-
-})
 
 location.reload(true)
 
