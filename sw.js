3fa8f24f0582ea67bb82e63ccb1e4939f3be00bb
losha24 <losha24@gmail.com>
2026-03-12 19:47:19 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,37 +1,36 @@
-const CACHE_NAME="mathcity-v4"
+const CACHE_NAME = 'mathcity-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/version.json',
+  '/manifest.json',
+  '/logo.png',
+  '/js/main.js',
+  '/js/game.js',
+  '/js/math.js',
+  '/js/shop.js',
+  '/js/rewards.js',
+  '/js/storage.js',
+  '/js/update.js'
+];
 
-const urls=[
-"./",
-"./index.html",
-"./style.css",
-"./game.js",
-"./logo.png"
-]
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
 
-self.addEventListener("install",e=>{
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+});
 
-e.waitUntil(
-
-caches.open(CACHE_NAME).then(cache=>{
-
-return cache.addAll(urls)
-
-})
-
-)
-
-})
-
-self.addEventListener("fetch",e=>{
-
-e.respondWith(
-
-caches.match(e.request).then(res=>{
-
-return res||fetch(e.request)
-
-})
-
-)
-
-})
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
