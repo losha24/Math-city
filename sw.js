055dac00826ae75835f8876867d2a052537ddff9
losha24 <losha24@gmail.com>
2026-03-12 20:28:40 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,19 @@
-const CACHE_NAME = 'mathcity-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/version.json',
-  '/manifest.json',
-  '/logo.png',
-  '/js/main.js'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open('mathcity-v1').then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/style.css',
+                '/js/main.js',
+                '/manifest.json'
+            ]);
+        })
+    );
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-    ))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(response => response || fetch(e.request))
+    );
 });
