a0a6ffdc598331f3978d031c8ae3a105d1a2b55e
losha24 <losha24@gmail.com>
2026-03-12 20:03:27 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'mathcity-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/version.json',
+  '/manifest.json',
+  '/logo.png',
+  '/js/main.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
