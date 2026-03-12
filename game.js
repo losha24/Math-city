fa08c14730bd31ad1957892d0dba5e5531bb75df
losha24 <losha24@gmail.com>
2026-03-12 11:09:14 +0200

    Update game.js

--- game.js
+++ game.js
@@ -5,41 +5,20 @@ let solved=0
 
 let questions=[]
 
-const city=document.getElementById("city")
+function startGame(){
 
-loadGame()
+document.getElementById("startScreen").style.display="none"
+document.getElementById("gameScreen").style.display="block"
 
-function saveGame(){
-
-localStorage.setItem("mathCity",JSON.stringify({
-
-score,
-coins,
-level,
-solved,
-city:city.innerHTML
-
-}))
-
-}
-
-function loadGame(){
-
-let data=localStorage.getItem("mathCity")
-
-if(data){
-
-let game=JSON.parse(data)
-
-score=game.score
-coins=game.coins
-level=game.level
-solved=game.solved
-city.innerHTML=game.city
-
-}
-
+generateQuestions()
 updateStats()
+
+}
+
+function resetGame(){
+
+localStorage.clear()
+location.reload()
 
 }
 
@@ -48,41 +27,23 @@ function newQuestion(){
 let a=Math.floor(Math.random()*10)
 let b=Math.floor(Math.random()*10)
 
-return{
-a:a,
-b:b,
-c:a+b
-}
+return {a:a,b:b,c:a+b}
 
 }
 
 function generateQuestions(){
 
 questions=[
-
 newQuestion(),
 newQuestion(),
 newQuestion(),
 newQuestion()
-
 ]
 
-document.getElementById("q1").innerText=
-questions[0].a+" + "+questions[0].b
-
-document.getElementById("q2").innerText=
-questions[1].a+" + "+questions[1].b
-
-document.getElementById("q3").innerText=
-questions[2].a+" + "+questions[2].b
-
-document.getElementById("q4").innerText=
-questions[3].a+" + "+questions[3].b
-
-document.getElementById("a1").value=""
-document.getElementById("a2").value=""
-document.getElementById("a3").value=""
-document.getElementById("a4").value=""
+document.getElementById("q1").innerText=questions[0].a+"+"+questions[0].b
+document.getElementById("q2").innerText=questions[1].a+"+"+questions[1].b
+document.getElementById("q3").innerText=questions[2].a+"+"+questions[2].b
+document.getElementById("q4").innerText=questions[3].a+"+"+questions[3].b
 
 }
 
@@ -90,15 +51,9 @@ function checkAnswers(){
 
 for(let i=1;i<=4;i++){
 
-let val=parseInt(
-document.getElementById("a"+i).value)
-
-let box=document.getElementById("a"+i)
+let val=parseInt(document.getElementById("a"+i).value)
 
 if(val===questions[i-1].c){
-
-box.classList.remove("red")
-box.classList.add("green")
 
 score++
 coins+=2
@@ -106,47 +61,15 @@ solved++
 
 }else{
 
-box.classList.add("red")
-box.classList.add("shake")
-
-setTimeout(()=>{
-
-box.classList.remove("shake")
-
-},300)
+document.getElementById("a"+i).classList.add("red")
 
 }
 
 }
-
-progressCity()
-
-updateStats()
-
-saveGame()
 
 generateQuestions()
 
-}
-
-function progressCity(){
-
-if(solved===3)build("🌳")
-if(solved===6)build("🏠")
-if(solved===10)build("🏪")
-if(solved===15)build("🏫")
-if(solved===20)build("🏢")
-if(solved===30)build("🎡")
-
-}
-
-function build(icon){
-
-let el=document.createElement("span")
-
-el.innerText=icon
-
-city.appendChild(el)
+updateStats()
 
 }
 
@@ -159,146 +82,36 @@ level=Math.floor(score/10)+1
 
 document.getElementById("level").innerText=level
 
-renderShop()
-
 }
 
 function spinWheel(){
 
-let rewards=[
+let r=Math.floor(Math.random()*3)
 
-{type:"coins",value:20},
-{type:"coins",value:50},
-{type:"build",value:"🏠"},
-{type:"build",value:"🌳"},
-{type:"score",value:10}
+if(r===0){coins+=20}
+if(r===1){score+=10}
+if(r===2){coins+=50}
 
-]
-
-let r=rewards[Math.floor(Math.random()*rewards.length)]
-
-if(r.type==="coins"){
-
-coins+=r.value
-alert("זכית ב "+r.value+" מטבעות")
-
-}
-
-if(r.type==="build"){
-
-build(r.value)
-alert("זכית בבניין")
-
-}
-
-if(r.type==="score"){
-
-score+=r.value
-alert("זכית ב "+r.value+" נקודות")
-
-}
+alert("קיבלת פרס!")
 
 updateStats()
-
-saveGame()
 
 }
 
 function dailyReward(){
 
-let today=new Date().toDateString()
-
-let last=localStorage.getItem("dailyReward")
-
-if(last===today){
-
-alert("כבר קיבלת מתנה היום")
-
-return
-
-}
-
 coins+=30
 
-alert("קיבלת 30 מטבעות 🎁")
-
-localStorage.setItem("dailyReward",today)
+alert("קיבלת מתנה יומית")
 
 updateStats()
 
-saveGame()
-
 }
 
-const shop=[
+function toggleShop(){
 
-{n:"עץ",p:10,i:"🌳"},
-{n:"בית",p:20,i:"🏠"},
-{n:"חנות",p:30,i:"🏪"},
-{n:"בית ספר",p:40,i:"🏫"},
-{n:"בניין",p:50,i:"🏢"},
-{n:"פארק",p:20,i:"🌲"},
-{n:"גלגל ענק",p:60,i:"🎡"},
-{n:"טירה",p:70,i:"🏰"},
-{n:"מגדל",p:80,i:"🗼"}
+let s=document.getElementById("shop")
 
-]
-
-function renderShop(){
-
-let html=""
-
-shop.forEach((item,i)=>{
-
-let color=coins>=item.p?"green":"red"
-
-html+=`
-
-<div class="shopItem">
-
-${item.i}
-
-<br>
-
-${item.n}
-
-<br>
-
-${item.p}
-
-<br>
-
-<button class="${color}"
-onclick="buy(${i})">
-
-קנה
-
-</button>
-
-</div>
-
-`
-
-})
-
-document.getElementById("shopItems").innerHTML=html
+s.style.display=s.style.display==="block"?"none":"block"
 
 }
-
-function buy(i){
-
-if(coins<shop[i].p)return
-
-coins-=shop[i].p
-
-build(shop[i].i)
-
-updateStats()
-
-saveGame()
-
-}
-
-generateQuestions()
-
-renderShop()
