a79325e37ef2e0b2654342589495e9bda45a1f66
losha24 <losha24@gmail.com>
2026-03-13 09:41:04 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,3 @@
-self.addEventListener('install', e=>{ e.waitUntil(caches.open('mathcity-v1').then(cache=>cache.addAll(['/','/index.html','/style.css','/js/main.js','/manifest.json'])));});
-self.addEventListener('fetch', e=>{ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+self.addEventListener('install', e => { e.waitUntil(self.skipWaiting()); });
+self.addEventListener('activate', e => { e.waitUntil(self.clients.claim()); });
+self.addEventListener('fetch', e => { e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))); });
