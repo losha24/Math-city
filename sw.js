598538dc2302774989ad1377bbf0d6f8f3d0a9cd
losha24 <losha24@gmail.com>
2026-03-13 19:30:36 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,7 @@
-const cacheName = 'mathcity-cache-v1';
-const filesToCache = [
-  '/',
-  '/index.html',
-  '/logo.png',
-  '/manifest.json'
-];
-
-// התקנת ה‑Service Worker ואחסון הקבצים
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
-  );
+  console.log('SW installed');
 });
 
-// הפעלת ה‑Service Worker לכל בקשת רשת
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request) // תחפש קודם ב‑cache
-      .then(response => response || fetch(event.request)) // אם לא קיים, הביא מהאינטרנט
-  );
+  // אפשר להוסיף קאשינג כאן
 });
